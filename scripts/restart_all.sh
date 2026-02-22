e0bcbb1baf9b1b44295da681541d712c09b851da
anagkan <109549575+anagkan@users.noreply.github.com>
2026-02-22 18:04:04 -0500

    Updated scripts and something else idk

--- scripts/restart_all.sh
+++ scripts/restart_all.sh
@@ -28,7 +28,7 @@ restart_service() {
 
 restart_service  "Traefik"  "/root/HomeLab/traefik" 5
 restart_service  "Authelia"  "/root/HomeLab/authelia"  5
-restart_service  "DDNS-Updater"  "/root/HomeLab/ddns" 5
+restart_service  "DDNS-Updater"  "/root/HomeLab/ddns_updater" 5
 restart_service  "BentoPDF"  "/root/HomeLab/bentopdf" 5
 restart_service  "PairDrop"  "/root/HomeLab/pairdrop" 10
 restart_service  "BookLore"  "/root/HomeLab/booklore" 20
